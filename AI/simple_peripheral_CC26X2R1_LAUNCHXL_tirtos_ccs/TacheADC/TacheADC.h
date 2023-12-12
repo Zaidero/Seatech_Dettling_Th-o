4dc48a26232039dd33e5da01d097e7464e7b24c7
Othmane <othmaneelbertal83@gmail.com>
2023-12-12 14:17:34 +0100

    Timer OK

---
+++ AI/simple_peripheral_CC26X2R1_LAUNCHXL_tirtos_ccs/TacheADC/TacheADC.h
@@ -0,0 +1,15 @@
+/*
+ * TacheADC.h
+ *
+ *  Created on: 8 déc. 2023
+ *      Author: GEII Robot
+ */
+
+#ifndef TACHEADC_TACHEADC_H_
+#define TACHEADC_TACHEADC_H_
+
+void TacheADC_taskFxn(UArg a0, UArg a1);
+void TacheADC_CreateTask(void);
+void myClockSwiFxn(uintptr_t arg0);
+
+#endif /* TACHEADC_TACHEADC_H_ */
